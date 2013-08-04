82d05c6e6b68af31d8dd7ea08dc5fb91fa970658
Philipp Sackl <philipp@envis-precisely.com>
2013-08-04 16:55:12 +0200

    added images and basic functionality

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,5 +1,14 @@
 //= require_tree .
 
 $(document).ready( function() {
-	
+	$('.switcher').click( function( e ) {
+    $('.all-tabs').toggleClass( 'overview' );
+  });
+
+  $('img').click( function( e ) {
+    var el = $( e.currentTarget );
+    $('img').removeClass('active');
+    el.addClass('active');
+    $('.all-tabs').removeClass('overview');
+  });
 });
