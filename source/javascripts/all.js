a767e46075055ec628cc3b82d779c3bb723eb329
Philipp Sackl <philipp@envis-precisely.com>
2013-08-05 22:28:59 +0200

    fix bug where tabs would always stay active

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -3,18 +3,25 @@
 $(document).ready( function() {
 	$('.switcher').click( function( e ) {
     $('.all-tabs').toggleClass( 'overview' );
+    setTimeout( function() {
+      $('.all-tabs').toggleClass( 'close-possible' );
+    }, 500);
     $('.searchfield input').focus();
   });
 
-  $('.overview img').click( function( e ) {
-    $('img').removeClass('active');
+  $('.overview .tab').click( function( e ) {
+    $('.tab').removeClass('active');
     $( e.currentTarget ).addClass('active');
     $('.all-tabs').removeClass('overview');
+    $('.all-tabs').removeClass( 'close-possible' );
     $('.searchfield input').focus();
   });
 
   $('img').dblclick( function( e ) {
     $('.all-tabs').toggleClass( 'overview' );
+    setTimeout( function() {
+      $('.all-tabs').toggleClass( 'close-possible' );
+    }, 500);
     $('.searchfield input').focus();
   });
 
